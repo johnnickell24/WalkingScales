c626204f4b8b3952ceab2e71f58374ed205bd9db
John Nickell <johnnickell24@gmail.com>
2018-08-08 21:13:36 -0500

    Add project files.

---
+++ CSharpSection/CSharpSection/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSection
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Universe!");
+
+            Console.WriteLine(Add(356, 564));
+
+            Console.ReadLine();
+        }
+
+        private static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
